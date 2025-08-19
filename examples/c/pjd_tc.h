c3d8ea1877f34f7bab8a19fda000d0e46a2aa648
Philip Duclos <pduclos@gogoair.com>
2025-08-19 10:18:53 -0600

    WIP

--- examples/c/pjd_tc.h
+++ examples/c/pjd_tc.h
@@ -8,8 +8,8 @@
 #endif
 
 struct endp_info {
-        __u32 src_ip;
-        __u32 dst_ip;
+        __u32 inside_ip;
+        __u32 outside_ip;
         __u32 in_count;
         __u32 out_count;
 };
