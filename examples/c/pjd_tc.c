601fb8ac4592f8f42a0c46feb165b08ed903ee76
Philip Duclos <pjd@clouds.com>
2025-08-28 16:49:24 +0000

    More betta

--- examples/c/pjd_tc.c
+++ examples/c/pjd_tc.c
@@ -38,12 +38,12 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va
 	return vfprintf(stderr, format, args);
 }
 
-/*
 static void print_endp_info(struct endp_info *endp) {
-	printf("endp_infop %p: inside ip %x, outside ip %x, in bytes %d, out bytes %d", endp,
+	fprintf(stderr, "endp_infop %p: inside ip %x, outside ip %x, in bytes %d, out bytes %d\n", endp,
 		endp->inside_ip, endp->outside_ip, endp->in_count, endp->out_count);
 }
 
+/*
 static void print_tc_hook(struct bpf_tc_hook *ptr) {
 	fprintf(stderr, "hook: sz: %d, ifindex: %d, attach_point: %d, parent:%d\n", (int)ptr->sz,
 		ptr->ifindex, (int)ptr->attach_point, (int)ptr->parent);
@@ -146,13 +146,13 @@ int main(int argc, char **argv)
 
 	//unsigned int key, prev_key = 0;
 	unsigned int key;
-	// struct endp_info *endp = NULL;
+	struct endp_info *endp = NULL;
 
 	sleep(1);
 	// if (syscall(__NR_bpf, BPF_MAP_GET_NEXT_KEY, 3, NULL, &prev_key) != 0) {
 	if (syscall(__NR_bpf, BPF_MAP_GET_NEXT_KEY, 3, NULL, &key) != 0) {
 		while (!exiting) {
-			sleep(1);
+			sleep(2);
 			union bpf_attr attrs = {
 			.map_fd = 3,
 			// .key = (unsigned long long)&prev_key,
@@ -160,6 +160,7 @@ int main(int argc, char **argv)
 			.next_key = (unsigned long long)&key,
 			// .value = (long long unsigned int)&endp,
 			};
+			union bpf_attr attrs2;
 
 			if (syscall(__NR_bpf, BPF_MAP_GET_NEXT_KEY, &attrs, sizeof(attrs)) != 0) {
 				fprintf(stderr, "pjd_tc: nextkey %x NOT found %d\n", key, errno);
@@ -167,20 +168,19 @@ int main(int argc, char **argv)
 				key = 0;
 				continue;
 			}
-			// fprintf(stderr, "pjd_tc: prev_key %x, key %x found\n", prev_key, key);
-			fprintf(stderr, "pjd_tc: key %x found\n", key);
+			// fprintf(stderr, "pjd_tc: key %x found\n", key);
 
-/*
-			endp = NULL;
-			if (syscall(__NR_bpf, BPF_MAP_LOOKUP_ELEM, &attrs, sizeof(attrs)) != 0) {
+			attrs2 = attrs;
+			attrs2.value = (long long unsigned int)&endp;
+			// endp = NULL;
+			if (syscall(__NR_bpf, BPF_MAP_LOOKUP_ELEM, &attrs2, sizeof(attrs2)) != 0) {
 				fprintf(stderr, "pjd_tc: Element lookup failed: endp %p ", endp);
 				continue;
 			}
-			if (endp != NULL) {
-				print_endp_info(endp);
+			if (attrs2.value) {
+				// fprintf(stderr, "pjd_tc: value %llx found\n", attrs2.value);
+				print_endp_info((struct endp_info *)attrs2.value);
 			}
-			prev_key = key;
-*/
 		}
 	}
 /*
